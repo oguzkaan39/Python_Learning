fe5b9717f5f5fa55bf11846cc0d391e30a373278
oguzkaan39 <oguz_kaan_gunduz@hotmail.com>
2017-03-05 00:18:20 +0200

    Use of print and input functions

---
+++ print1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#Kullanıcıdan string türünde bir veri alma ve aldığı veriyi isim değişkenine atama işlemi
+isim = input("Adınızı Giriniz : ")
+#isim değişkenini ekrana yazdırma.
+print("Girdiğiniz isim:",isim)
